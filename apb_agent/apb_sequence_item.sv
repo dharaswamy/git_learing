6745ca2d6fc27862589f7fa60a64bceb950bf037
dharaswamy <dharaswamy143y@gmail.com>
2025-03-01 16:50:37 +0530

    Added apb_sequence_item template
    Added apb_sequence_item template .
    1.Defined the apb_sequence_item class 
    2.Factory registration
    3.Defined Default constructor

---
+++ apb_agent/apb_sequence_item.sv
@@ -0,0 +1,19 @@
+
+/*
+File Name : apb_sequence_item.sv 
+Purpose   : It is a object class , it contains the data, address and all apb variables and it generates the random values .
+Author    : dharaswamy
+Revision  : Version A 
+
+*/
+
+class apb_sequence_item extends uvm_sequence_item ;
+  //Factory registeration
+  uvm_object_utils(apb_sequence_item)
+
+  // function Name : new(), Default constructor 
+  function new(string name = "apb_sequence_item");
+    super.new(name);
+  endfunction:new 
+  
+endclass:apb_sequence_item 

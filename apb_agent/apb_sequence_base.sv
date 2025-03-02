45e62c81eae4b5ed913f2e077bc0f20cf085aca9
dharaswamy <dharaswamy143y@gmail.com>
2025-03-02 12:21:37 +0530

    Added apb_sequence_base template

---
+++ apb_agent/apb_sequence_base.sv
@@ -0,0 +1,26 @@
+
+/* 
+File Name : apb_sequence_base.sv
+Purpose   : It is base sequence for all apb sequences. 
+
+*/
+
+
+class apb_sequence_base extends uvm_sequence#(apb_sequence_item); 
+
+  // Factory Registration 
+    uvm_object_utils(apb_sequence_base) 
+  //Function Name : new()-Default Constructor
+    function new(string name = "apb_sequence_base");
+      super.new(name);
+    endfunction:new
+
+  // Function Name : body() 
+    virtual task body();
+     req = apb_sequence_item::type_id::create("this");
+     req.randomize();
+    endtask:body
+
+endclass:apb_sequence_base
+
+

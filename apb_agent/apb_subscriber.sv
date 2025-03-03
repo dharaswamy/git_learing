4aa3e4c3cb354749f32d5a63e5e1172ebdebae2a
dharaswamy <dharaswamy143y@gmail.com>
2025-03-03 12:56:47 +0530

    Added Basic Template of apb_subscriber

---
+++ apb_agent/apb_subscriber.sv
@@ -0,0 +1,17 @@
+
+/*
+
+File Name : apb_subscriber.sv
+Purpose   : It measure functional coverage of apb protocol .
+
+*/
+
+class apb_subscriber extends uvm_subscriber#(apb_sequence_item);
+
+  //Factory Registration
+   uvm_component_utils(apb_subscriber) 
+
+
+endclass:apb_subscriber
+
+

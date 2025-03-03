9fd8b94edf423416be9426f6bb8f87123738d525
dharaswamy <dharaswamy143y@gmail.com>
2025-03-03 11:46:11 +0530

    Added start_item(req)

--- apb_agent/apb_sequence_base.sv
+++ apb_agent/apb_sequence_base.sv
@@ -19,6 +19,7 @@ class apb_sequence_base extends uvm_sequence#(apb_sequence_item);
     virtual task body();
      req = apb_sequence_item::type_id::create("this");
      req.randomize()with{addr == 10;} ;
+     start_item(req);
     endtask:body
 
 endclass:apb_sequence_base
